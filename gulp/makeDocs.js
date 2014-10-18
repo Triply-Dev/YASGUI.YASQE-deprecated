3888950e1f160de2b6384db4a38185d88b9f4b3b
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-10-18 05:44:24 +0200

    #20: refactored doc generation to static

--- gulp/makeDocs.js
+++ gulp/makeDocs.js
@@ -1,25 +1,20 @@
 var gulp = require('gulp'),
 	browserify = require('browserify'),
-	notify = require("gulp-notify"),
 	jsValidate = require('gulp-jsvalidate'),
 	source = require('vinyl-source-stream'),
 	embedlr = require('gulp-embedlr'),
-	concat = require('gulp-concat'),
-	yuidoc = require("gulp-yuidoc");
+	concat = require('gulp-concat');
 
-
-gulp.task('makeDocLib', function() {
+gulp.task('makeMainPageJs', function() {
 	return gulp.src("./doc/*.js").pipe(jsValidate()).on('finish', function(){
 				browserify({entries: ["./doc/main.js"],debug: true})
-				.bundle().on('prebundle', function(bundle) {
-			    	  console.log("prebundle!");
-			    	})
+				.bundle()
 			    .pipe(source('doc.js'))
 			    .pipe(embedlr())
 			    .pipe(gulp.dest('doc'));
 			});
 });
-gulp.task('makeDocCss', function() {
+gulp.task('makeMainPageCss', function() {
 	return gulp.src(['node_modules/twitter-bootstrap-3.0.0/dist/css/bootstrap.css', './doc/main.css'])
   	.pipe(concat('doc.css'))
     .pipe(gulp.dest("doc"))
@@ -27,8 +22,4 @@ gulp.task('makeDocCss', function() {
 	
 });
 
-gulp.task('makedoc', ['makeDocLib', 'makeDocCss'], function() {
-	gulp.src("./src/main.js")
-	.pipe(yuidoc.parser())
-	.pipe(gulp.dest("./doc"));
-});
+gulp.task('makeMainPage', ['makeMainPageJs', 'makeMainPageCss']);
--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,6 @@
 require('require-dir')('./gulp');
 var gulp = require('gulp');
 
-gulp.task('default', ['browserify', 'browserifyWithDeps', 'minifyCss', 'makedoc']);
+gulp.task('default', ['browserify', 'browserifyWithDeps', 'minifyCss', 'makeMainPage']);
 gulp.task('serve', ['default', 'minifyCss', 'watch', 'connect']);
 
