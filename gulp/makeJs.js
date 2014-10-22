d061f3c8d37f9d1e3a6eed97a6cb6450b8b603b1
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-10-22 15:09:19 +0200

    fixed #14: added map files for all bundles

--- gulp/makeJs.js
+++ gulp/makeJs.js
@@ -9,7 +9,10 @@ var gulp = require('gulp'),
 	rename = require("gulp-rename"),
 	streamify = require('gulp-streamify'),
 	shim = require('browserify-shim'),
-	paths = require("./paths.js");
+	paths = require("./paths.js"),
+	buffer = require('vinyl-buffer'),
+	exorcist = require('exorcist'),
+	sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task('browserify', function() {
@@ -19,25 +22,61 @@ gulp.task('browserify', function() {
 		.exclude('codemirror')
 		.exclude('../../lib/codemirror') 
 		.bundle()
+		.pipe(exorcist(paths.bundleDir + '/' + paths.bundleName + '.js.map'))
 		.pipe(source(paths.bundleName + '.js'))
-		.pipe(streamify(jsValidate()))
 		.pipe(gulp.dest(paths.bundleDir))
 		.pipe(rename(paths.bundleName + '.min.js'))
-		.pipe(streamify(uglify()))
-		.pipe(gulp.dest(paths.bundleDir))
-		.pipe(connect.reload());
-	
+		.pipe(buffer())
+		.pipe(sourcemaps.init({
+			loadMaps: true,
+			//we need to -not- include the content. Break-points only work when using this particular trick
+			includeContent: false,
+		      sourceRoot: function(file) {
+		        return './';
+		      }
+		}))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(paths.bundleDir));
 });
+
 
 
 gulp.task('browserifyWithDeps', function() {
-	browserify({entries: ["./src/main.js"],standalone: "YASQE", debug: true})
+	var bundler = browserify({entries: ["./src/main.js"],standalone: "YASQE", debug: true});
+	
+	return bundler
 		.bundle()
+		.pipe(exorcist(paths.bundleDir + '/' + paths.bundleName + '.bundled.js.map'))
 		.pipe(source(paths.bundleName + '.bundled.js'))
-		.pipe(embedlr())
 		.pipe(gulp.dest(paths.bundleDir))
 		.pipe(rename(paths.bundleName + '.bundled.min.js'))
-		.pipe(streamify(uglify()))
+		.pipe(buffer())
+		.pipe(sourcemaps.init({
+			loadMaps: true,
+			//we need to -not- include the content. Break-points only work when using this particular trick
+			includeContent: false,
+		      sourceRoot: function(file) {
+		        return './';
+		      }
+		}))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(paths.bundleDir));
+});
+
+
+/**
+ * Faster, because we don't minify, and include source maps in js file (notice we store it with .min.js extension, so we don't have to change the index.html file for debugging)
+ */
+gulp.task('browserifyForDebug', function() {
+	var bundler = browserify({entries: ["./src/main.js"],standalone: "YASQE", debug: true});
+	
+	return bundler
+		.bundle()
+		.pipe(source(paths.bundleName + '.bundled.min.js'))
+		.pipe(embedlr())
 		.pipe(gulp.dest(paths.bundleDir))
 		.pipe(connect.reload());
 });
+
--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,6 @@
 require('require-dir')('./gulp');
-var gulp = require('gulp');
 
+var gulp = require('gulp');
 gulp.task('default', ['browserify', 'browserifyWithDeps', 'minifyCss', 'makeMainPage']);
-gulp.task('serve', ['default', 'watch', 'connect']);
+gulp.task('serve', ['minifyCss', 'makeMainPage', 'browserifyForDebug', 'watch', 'connect']);
 
