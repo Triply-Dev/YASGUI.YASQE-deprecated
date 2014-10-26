880a5a955320d04fcf7b968959dcbf97b3b645b2
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-10-26 14:44:30 +0100

    moved function to utils

--- src/main.js
+++ src/main.js
@@ -197,7 +197,7 @@ var postProcessCmElement = function(yasqe) {
 var updateButtonsTransparency = function(yasqe) {
 	yasqe.cursor = $(".CodeMirror-cursor");
 	if (yasqe.buttons && yasqe.buttons.is(":visible") && yasqe.cursor.length > 0) {
-		if (elementsOverlap(yasqe.cursor, yasqe.buttons)) {
+		if (utils.elementsOverlap(yasqe.cursor, yasqe.buttons)) {
 			yasqe.buttons.find("svg").attr("opacity", "0.2");
 		} else {
 			yasqe.buttons.find("svg").attr("opacity", "1.0");
@@ -206,28 +206,6 @@ var updateButtonsTransparency = function(yasqe) {
 };
 
 
-var elementsOverlap = (function () {
-    function getPositions( elem ) {
-        var pos, width, height;
-        pos = $( elem ).offset();
-        width = $( elem ).width();
-        height = $( elem ).height();
-        return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
-    }
-
-    function comparePositions( p1, p2 ) {
-        var r1, r2;
-        r1 = p1[0] < p2[0] ? p1 : p2;
-        r2 = p1[0] < p2[0] ? p2 : p1;
-        return r1[1] > r2[0] || r1[0] === r2[0];
-    }
-
-    return function ( a, b ) {
-        var pos1 = getPositions( a ),
-            pos2 = getPositions( b );
-        return comparePositions( pos1[0], pos2[0] ) && comparePositions( pos1[1], pos2[1] );
-    };
-})();
 
 
 
