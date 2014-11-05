e9d1a8ccdd3addecc3fd1ca14a68b47356e89588
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-11-05 23:19:02 +0100

    updated doc pages

--- doc/main.js
+++ doc/main.js
@@ -3,3 +3,18 @@ $ = jQuery = require("jquery");
 require("../node_modules/twitter-bootstrap-3.0.0/dist/js/bootstrap.js");
 
 
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 400);
+        return false;
+      }
+    }
+  });
+});
