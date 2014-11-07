e1269df29d8ce35575c67a811848188f93f02776
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-11-07 18:35:52 +0100

    fixed #37 minify doc files

--- gulp/makeMainPage.js
+++ gulp/makeMainPage.js
@@ -3,20 +3,25 @@ var gulp = require('gulp'),
 	jsValidate = require('gulp-jsvalidate'),
 	source = require('vinyl-source-stream'),
 	embedlr = require('gulp-embedlr'),
+	minifyCSS = require('gulp-minify-css'),
+	uglify = require("gulp-uglify"),
+	buffer = require("vinyl-buffer"),
 	concat = require('gulp-concat');
 
 gulp.task('makeMainPageJs', function() {
 	return gulp.src("./doc/*.js").pipe(jsValidate()).on('finish', function(){
 				browserify({entries: ["./doc/main.js"],debug: true})
 				.bundle()
-			    .pipe(source('doc.js'))
-			    .pipe(embedlr())
+				.pipe(source('doc.min.js'))
+				.pipe(buffer())
+				.pipe(uglify())
 			    .pipe(gulp.dest('doc'));
 			});
 });
 gulp.task('makeMainPageCss', function() {
 	return gulp.src(['node_modules/twitter-bootstrap-3.0.0/dist/css/bootstrap.css', './doc/main.css'])
-  	.pipe(concat('doc.css'))
+  	.pipe(concat('doc.min.css'))
+  	.pipe(minifyCSS())
     .pipe(gulp.dest("doc"))
     ;
 	
