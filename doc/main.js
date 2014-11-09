90fb5df160e970b230c3cabe0ef71ccb0d4cd535
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-11-09 21:00:15 +0100

    \#22 integrated gists into documentation (dynamically fetches from github)

--- doc/main.js
+++ doc/main.js
@@ -39,5 +39,48 @@ $(document).ready(function() {
 			$("#cdnDownload").hide();
 		});
 	}
+	var gistContainer = $("#gistContainer");
+	if (gistContainer.length > 0) {
+		$.get("https://api.github.com/users/LaurensRietveld/gists", function(data) {
+			var processLabel = function(origLabel) {
+				var label = origLabel.replace("#YASQE", "YASQE");
+				label = label.replace("#YASR", "YASR");
+				var splitted = label.split(" ");
+				if (splitted.length > 0) {
+					if ((splitted[0].indexOf("YASQE") == 0 || splitted[0].indexOf("YASR") == 0) && splitted[0].slice(-1) == ":") {
+						//we want to change "#YASQE: some gist" into "some gist". So, remove the first item
+						return splitted.splice(1,1).join(" ");
+					} else {
+						return splitted.join(" ");
+					}
+				} else {
+					return label;
+				}
+			};
+			data.forEach(function(gist) {
+				if (gist.description.indexOf("#YASQE") >= 0) {
+					var gistDiv = $("<div>").addClass("gist").appendTo(gistContainer);
+					$("<h4>").text(processLabel(gist.description)).appendTo(gistDiv);
+					if (gist.files["README.md"]) {
+						var description = $("<p>").appendTo(gistDiv);
+						$.get(gist.url, function(gistFile) {
+							description.text(gistFile.files["README.md"].content);
+						});
+					}
+					var buttonContainer = $("<p>").appendTo(gistDiv);
+					$("<a class='btn btn-primary btn-sm' target='_blank' href='#' role='button'>Demo</a>").attr('href', 'http://bl.ocks.org/LaurensRietveld/raw/' + gist.id).appendTo(buttonContainer);
+					$("<a style='margin-left: 4px;' target='_blank' class='btn btn-default btn-sm' href='#' role='button'>Code <img class='pull-right gistIcon' src='imgs/blacktocat_black.png'></a>").attr('href', gist["html_url"]).appendTo(buttonContainer);
+				}
+			});
+		});
+		
+		
+//		<div class="gist">
+//		<h4>LABELLL</h4>
+//		<p>description</p>
+// 		<p><a class="btn btn-primary btn-sm" href="#" role="button">Demo</a> <a class="btn btn-default btn-sm" href="#" role="button">Code <img class="pull-right gistIcon" src="imgs/blacktocat_black.png"></a></p>
+//		</div>
+		
+	}
 });
 
