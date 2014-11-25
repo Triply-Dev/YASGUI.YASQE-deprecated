0634032a144ba4c6e2a42a2b3782aa6b9551601d
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-11-25 11:43:54 +0100

    moved logic of style concatenation from gulp to sass, to better support re-use in other components

--- gulp/paths.js
+++ gulp/paths.js
@@ -1,11 +1,5 @@
 module.exports =  {
-	style: [
-		'node_modules/codemirror/lib/codemirror.css', 
-		'node_modules/codemirror/addon/hint/show-hint.css',
-		'node_modules/codemirror/addon/display/fullscreen.css',
-		'node_modules/codemirror/addon/fold/foldgutter.css', 
-		'src/**/*.scss',
-	],
+	style: ['src/scss/scoped.scss', 'src/scss/global.scss'],
 	bundleDir: "dist",
 	bundleName: "yasqe",
 	docDir: "doc"
