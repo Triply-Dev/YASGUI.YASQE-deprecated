d3e935b01afe3bbd6460353e96ea5332eac99776
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-12-01 00:35:33 +0100

    added workaround for issue in css minifier

--- gulp/makeCss.js
+++ gulp/makeCss.js
@@ -11,12 +11,18 @@ var gulp = require('gulp'),
 
 gulp.task('makeCss', function() {
 	  return gulp.src(paths.style)
-		  .pipe(cssImport())//needed, because css files are not -actually- imported by sass, but remain as css @import statement...
+		.pipe(cssImport())//needed, because css files are not -actually- imported by sass, but remain as css @import statement...
 	    .pipe(sass())
 	    .pipe(concat(paths.bundleName + '.css'))
 	    .pipe(gulp.dest(paths.bundleDir))
-	    .pipe(minifyCSS())
 	    .pipe(rename(paths.bundleName + '.min.css'))
+	    .pipe(minifyCSS({
+			//the minifyer does not work well with lines including a comment. e.g.
+			///* some comment */ } 
+			//is completely removed (including the final bracket)
+			//So, disable the 'advantaced' feature. This only makes the minified file 100 bytes larger
+			noAdvanced: true, 
+		}))
 	    .pipe(gulp.dest(paths.bundleDir))
 	    .pipe(connect.reload());
 })
